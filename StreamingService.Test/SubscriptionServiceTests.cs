3712329021f4711388e55595765cae8ce15a1664
Dan Stow <Dan_Stow@live.co.uk>
2022-08-22 21:01:14 +0100

    Create SubscriptionServiceTests.cs

---
+++ StreamingService.Test/SubscriptionServiceTests.cs
@@ -0,0 +1,52 @@
+﻿using StreamingService.Services;
+using System;
+using Xunit;
+using System.Linq;
+
+namespace StreamingService.Test
+{
+    /// <summary>
+    /// Test suite for <see cref="SubscriptionService"/>.
+    /// </summary>
+    public class SubscriptionServiceTests
+    {
+        private readonly SubscriptionService SUT;
+
+        private readonly Guid freemiumGuid = Guid.NewGuid();
+        private readonly Guid premiumGuid = Guid.NewGuid();
+        private readonly Guid unlimitedGuid = Guid.NewGuid();
+
+        public SubscriptionServiceTests()
+        {
+            var mockSubscriptionRepo = new MockRepositories.MockSubscriptionRepository();
+            mockSubscriptionRepo.Add(new Models.Subscription()
+            {
+                Id = freemiumGuid,
+                Package = Models.Packages.Freemium,
+                Price = 0f
+            });
+            mockSubscriptionRepo.Add(new Models.Subscription()
+            {
+                Id = premiumGuid,
+                Package = Models.Packages.Premium,
+                Price = 4.99f
+            });
+            mockSubscriptionRepo.Add(new Models.Subscription()
+            {
+                Id = unlimitedGuid,
+                Package = Models.Packages.Unlimitted,
+                Price = 9.99f
+            });
+
+            SUT = new SubscriptionService(mockSubscriptionRepo);
+        }
+
+        [Fact]
+        public void GetById()
+        {
+            var result = SUT.GetById(premiumGuid);
+
+            Assert.Equal(premiumGuid, result.Id);
+        }
+    }
+}
